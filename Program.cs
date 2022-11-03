496cd1513c41b8e67d82dd7568caa00008a5a50b
Trainee <super.sel12@yadnex.ru>
2022-11-03 14:27:35 +0300

    .\Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+ string[] ArrayFill;
+            Console.WriteLine("Введите массив строк через пробел");
+            string line = Console.ReadLine();
+            ArrayFill = line.Split(' ');
+            var result = new string[ArrayFill .Length];
+            var Size = 0;
+         
